cababbb4a03902dcf8f555c2187e2d97fe439c1f
Andrey Vashchenko <andrey.vashchenko@bytepace.com>
2019-07-30 12:08:57 +0600

    [add] new app icon [add] material style [add] transitions

--- app/src/main/java/com/bytepace/transitions/MainActivity.kt
+++ app/src/main/java/com/bytepace/transitions/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.bytepace.transitions
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
+import android.transition.Explode
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java),
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
     }
 }
